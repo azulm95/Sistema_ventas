fe80de59c620295d554a2837a0e35ebe511e3e5d
luis <darkcry1992@gmail.com>
2015-09-24 05:43:06 -0500

    comit-segundo

---
+++ Sisventas/Sistema_ventas/formularios/MenuPrincipal.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Sistema_ventas.formularios
+{
+    partial class MenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuPrincipal));
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.lblnombre = new System.Windows.Forms.ToolStripLabel();
+            this.lblrol = new System.Windows.Forms.ToolStripLabel();
+            this.Menu = new System.Windows.Forms.ToolStrip();
+            this.btnsalir = new System.Windows.Forms.ToolStripButton();
+            this.btnconfiguracion = new System.Windows.Forms.ToolStripButton();
+            this.btnInventario = new System.Windows.Forms.Button();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.btnCompras = new System.Windows.Forms.Button();
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(0, 22);
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(12, 22);
+            this.lblnombre.Text = "-";
+            // 
+            // lblrol
+            // 
+            this.lblrol.Name = "lblrol";
+            this.lblrol.Size = new System.Drawing.Size(12, 22);
+            this.lblrol.Text = "-";
+            // 
+            // Menu
+            // 
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.lblnombre,
+            this.lblrol,
+            this.btnsalir,
+            this.btnconfiguracion});
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(549, 25);
+            this.Menu.TabIndex = 0;
+            this.Menu.Text = "Menu";
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnsalir.Image = ((System.Drawing.Image)(resources.GetObject("btnsalir.Image")));
+            this.btnsalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(23, 22);
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // btnconfiguracion
+            // 
+            this.btnconfiguracion.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnconfiguracion.Image = ((System.Drawing.Image)(resources.GetObject("btnconfiguracion.Image")));
+            this.btnconfiguracion.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnconfiguracion.Name = "btnconfiguracion";
+            this.btnconfiguracion.Size = new System.Drawing.Size(23, 22);
+            this.btnconfiguracion.Text = "Configuracion";
+            // 
+            // btnInventario
+            // 
+            this.btnInventario.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnInventario.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInventario.Image = ((System.Drawing.Image)(resources.GetObject("btnInventario.Image")));
+            this.btnInventario.Location = new System.Drawing.Point(58, 109);
+            this.btnInventario.Name = "btnInventario";
+            this.btnInventario.Size = new System.Drawing.Size(97, 91);
+            this.btnInventario.TabIndex = 1;
+            this.btnInventario.Text = "Inventario";
+            this.btnInventario.UseVisualStyleBackColor = true;
+            this.btnInventario.Click += new System.EventHandler(this.btnInventario_Click);
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVentas.Image = ((System.Drawing.Image)(resources.GetObject("btnVentas.Image")));
+            this.btnVentas.Location = new System.Drawing.Point(161, 109);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.Size = new System.Drawing.Size(106, 91);
+            this.btnVentas.TabIndex = 2;
+            this.btnVentas.Text = "Ventas";
+            this.btnVentas.UseVisualStyleBackColor = true;
+            this.btnVentas.Click += new System.EventHandler(this.btnVentas_Click);
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReportes.Image = ((System.Drawing.Image)(resources.GetObject("btnReportes.Image")));
+            this.btnReportes.Location = new System.Drawing.Point(399, 110);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Size = new System.Drawing.Size(101, 90);
+            this.btnReportes.TabIndex = 4;
+            this.btnReportes.Text = "Reportes";
+            this.btnReportes.UseVisualStyleBackColor = true;
+            // 
+            // btnCompras
+            // 
+            this.btnCompras.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompras.Image = ((System.Drawing.Image)(resources.GetObject("btnCompras.Image")));
+            this.btnCompras.Location = new System.Drawing.Point(274, 109);
+            this.btnCompras.Name = "btnCompras";
+            this.btnCompras.Size = new System.Drawing.Size(119, 91);
+            this.btnCompras.TabIndex = 6;
+            this.btnCompras.Text = "Compras a Proveedores";
+            this.btnCompras.UseVisualStyleBackColor = true;
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 253);
+            this.Controls.Add(this.btnCompras);
+            this.Controls.Add(this.btnReportes);
+            this.Controls.Add(this.btnVentas);
+            this.Controls.Add(this.btnInventario);
+            this.Controls.Add(this.Menu);
+            this.Name = "MenuPrincipal";
+            this.Text = "d";
+            this.Load += new System.EventHandler(this.MenuPrincipal_Load);
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel lblnombre;
+        private System.Windows.Forms.ToolStripLabel lblrol;
+        private System.Windows.Forms.ToolStrip Menu;
+        private System.Windows.Forms.Button btnInventario;
+        private System.Windows.Forms.Button btnVentas;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.ToolStripButton btnsalir;
+        private System.Windows.Forms.ToolStripButton btnconfiguracion;
+        private System.Windows.Forms.Button btnCompras;
+
+    }
+}
