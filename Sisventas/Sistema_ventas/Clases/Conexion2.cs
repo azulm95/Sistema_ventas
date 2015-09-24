4e03a91ccefcb56546ce621f81b4384de4f29216
luis <darkcry1992@gmail.com>
2015-09-24 05:40:33 -0500

    Revert "segundo commit"
    This reverts commit d7b547399b6aba3f3a279f3d6dec7fa55b515b11.

---
+++ Sisventas/Sistema_ventas/Clases/Conexion2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sistema_ventas.datos;
+namespace Sistema_ventas.Clases
+{
+    class Conexion2{
+        private postEntities basedatos = new postEntities();
+        private String respuesta = " ";
+
+        public void Insertar(String tabla, String[] registro){
+            switch (tabla){
+                    case "usuarios":
+                        tbl_usuarios usuario = new tbl_usuarios();
+
+                        usuario.nombre = registro[0];
+                        usuario.clave = registro[1];
+                        usuario.rol = registro[2];
+                        basedatos.tbl_usuarios.Add(usuario);
+                        basedatos.SaveChanges();
+                        respuesta = "Usuario creado con exito!";
+                        break;
+                       
+
+                    default:
+                        respuesta = " ";
+                        break;
+                }
+        }
+
+        public Boolean Loguin(String nombre,String clave){
+            Boolean resultado=  false;
+            var query = from p in basedatos.tbl_usuarios
+                        where p.nombre == nombre && p.clave == clave
+                        select p.nombre.FirstOrDefault();
+
+            if (query != null){
+                resultado = true;
+            }else {
+                resultado = false;
+            }
+
+
+            return resultado;
+        }
+        
+       
+
+            
+
+
+
+
+          
+ 
+
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+}
+
+}
