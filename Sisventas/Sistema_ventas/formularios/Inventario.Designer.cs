0b2c8bc4e199d2939d6a5874bc5473f11c3525ed
azulm95 <azulm95@gmail.com>
2015-09-24 05:26:26 -0500

    segundo comit

---
+++ Sisventas/Sistema_ventas/formularios/Inventario.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace Sistema_ventas.formularios
+{
+    partial class Inventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inventario));
+            this.tpinventario2 = new System.Windows.Forms.TabControl();
+            this.tpinvntario1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.Menu = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.lblnombre = new System.Windows.Forms.ToolStripLabel();
+            this.lblrol = new System.Windows.Forms.ToolStripLabel();
+            this.btnsalir = new System.Windows.Forms.ToolStripButton();
+            this.btnconfiguracion = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtproducto = new System.Windows.Forms.TextBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.gproductos = new System.Windows.Forms.GroupBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.dtfecha_ven = new System.Windows.Forms.DateTimePicker();
+            this.txtdescripcion = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbxmarca = new System.Windows.Forms.ComboBox();
+            this.cbxclasificacion = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnAgragar = new System.Windows.Forms.Button();
+            this.tpinventario2.SuspendLayout();
+            this.tpinvntario1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.Menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.gproductos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tpinventario2
+            // 
+            this.tpinventario2.Controls.Add(this.tpinvntario1);
+            this.tpinventario2.Controls.Add(this.tabPage2);
+            this.tpinventario2.Location = new System.Drawing.Point(-2, 36);
+            this.tpinventario2.Name = "tpinventario2";
+            this.tpinventario2.SelectedIndex = 0;
+            this.tpinventario2.Size = new System.Drawing.Size(814, 407);
+            this.tpinventario2.TabIndex = 0;
+            // 
+            // tpinvntario1
+            // 
+            this.tpinvntario1.Controls.Add(this.label1);
+            this.tpinvntario1.Controls.Add(this.txtproducto);
+            this.tpinvntario1.Controls.Add(this.btnbuscar);
+            this.tpinvntario1.Controls.Add(this.dataGridView1);
+            this.tpinvntario1.Location = new System.Drawing.Point(4, 22);
+            this.tpinvntario1.Name = "tpinvntario1";
+            this.tpinvntario1.Padding = new System.Windows.Forms.Padding(3);
+            this.tpinvntario1.Size = new System.Drawing.Size(806, 381);
+            this.tpinvntario1.TabIndex = 0;
+            this.tpinvntario1.Text = "Inventario";
+            this.tpinvntario1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.gproductos);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(806, 381);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Agregar productos";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.lblnombre,
+            this.lblrol,
+            this.btnsalir,
+            this.btnconfiguracion});
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(812, 25);
+            this.Menu.TabIndex = 1;
+            this.Menu.Text = "Menu";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(0, 22);
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(12, 22);
+            this.lblnombre.Text = "-";
+            // 
+            // lblrol
+            // 
+            this.lblrol.Name = "lblrol";
+            this.lblrol.Size = new System.Drawing.Size(12, 22);
+            this.lblrol.Text = "-";
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnsalir.Image = ((System.Drawing.Image)(resources.GetObject("btnsalir.Image")));
+            this.btnsalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(23, 22);
+            this.btnsalir.Text = "Salir";
+            // 
+            // btnconfiguracion
+            // 
+            this.btnconfiguracion.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnconfiguracion.Image = ((System.Drawing.Image)(resources.GetObject("btnconfiguracion.Image")));
+            this.btnconfiguracion.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnconfiguracion.Name = "btnconfiguracion";
+            this.btnconfiguracion.Size = new System.Drawing.Size(23, 22);
+            this.btnconfiguracion.Text = "Configuracion";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Digite el nombre del prooducto:";
+            // 
+            // txtproducto
+            // 
+            this.txtproducto.Location = new System.Drawing.Point(169, 9);
+            this.txtproducto.Name = "txtproducto";
+            this.txtproducto.Size = new System.Drawing.Size(194, 20);
+            this.txtproducto.TabIndex = 10;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Location = new System.Drawing.Point(378, 9);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(101, 24);
+            this.btnbuscar.TabIndex = 9;
+            this.btnbuscar.Text = "buscar";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(804, 306);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // gproductos
+            // 
+            this.gproductos.Controls.Add(this.btnAgragar);
+            this.gproductos.Controls.Add(this.label8);
+            this.gproductos.Controls.Add(this.cbxclasificacion);
+            this.gproductos.Controls.Add(this.cbxmarca);
+            this.gproductos.Controls.Add(this.label7);
+            this.gproductos.Controls.Add(this.label6);
+            this.gproductos.Controls.Add(this.label5);
+            this.gproductos.Controls.Add(this.label4);
+            this.gproductos.Controls.Add(this.label3);
+            this.gproductos.Controls.Add(this.label2);
+            this.gproductos.Controls.Add(this.txtdescripcion);
+            this.gproductos.Controls.Add(this.dtfecha_ven);
+            this.gproductos.Controls.Add(this.txtprecio);
+            this.gproductos.Controls.Add(this.txtcodigo);
+            this.gproductos.Controls.Add(this.txtnombre);
+            this.gproductos.Location = new System.Drawing.Point(6, 2);
+            this.gproductos.Name = "gproductos";
+            this.gproductos.Size = new System.Drawing.Size(800, 385);
+            this.gproductos.TabIndex = 0;
+            this.gproductos.TabStop = false;
+            this.gproductos.Text = "Nombre:";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(102, 58);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(200, 20);
+            this.txtnombre.TabIndex = 0;
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(102, 107);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(200, 20);
+            this.txtcodigo.TabIndex = 1;
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(496, 58);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(200, 20);
+            this.txtprecio.TabIndex = 3;
+            // 
+            // dtfecha_ven
+            // 
+            this.dtfecha_ven.Location = new System.Drawing.Point(522, 104);
+            this.dtfecha_ven.Name = "dtfecha_ven";
+            this.dtfecha_ven.Size = new System.Drawing.Size(200, 20);
+            this.dtfecha_ven.TabIndex = 4;
+            // 
+            // txtdescripcion
+            // 
+            this.txtdescripcion.Location = new System.Drawing.Point(125, 217);
+            this.txtdescripcion.Multiline = true;
+            this.txtdescripcion.Name = "txtdescripcion";
+            this.txtdescripcion.Size = new System.Drawing.Size(648, 103);
+            this.txtdescripcion.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Codigo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Marca:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(420, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Precio:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(18, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Descripcion:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(331, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(185, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Fecha de  vencinmiento:";
+            // 
+            // cbxmarca
+            // 
+            this.cbxmarca.FormattingEnabled = true;
+            this.cbxmarca.Location = new System.Drawing.Point(102, 161);
+            this.cbxmarca.Name = "cbxmarca";
+            this.cbxmarca.Size = new System.Drawing.Size(200, 21);
+            this.cbxmarca.TabIndex = 12;
+            // 
+            // cbxclasificacion
+            // 
+            this.cbxclasificacion.FormattingEnabled = true;
+            this.cbxclasificacion.Location = new System.Drawing.Point(496, 161);
+            this.cbxclasificacion.Name = "cbxclasificacion";
+            this.cbxclasificacion.Size = new System.Drawing.Size(200, 21);
+            this.cbxclasificacion.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(383, 161);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Clasificacion:";
+            // 
+            // btnAgragar
+            // 
+            this.btnAgragar.BackColor = System.Drawing.Color.Silver;
+            this.btnAgragar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgragar.Location = new System.Drawing.Point(685, 334);
+            this.btnAgragar.Name = "btnAgragar";
+            this.btnAgragar.Size = new System.Drawing.Size(109, 39);
+            this.btnAgragar.TabIndex = 15;
+            this.btnAgragar.Text = "Ingresar";
+            this.btnAgragar.UseVisualStyleBackColor = false;
+            this.btnAgragar.Click += new System.EventHandler(this.btnAgragar_Click);
+            // 
+            // Inventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 441);
+            this.Controls.Add(this.Menu);
+            this.Controls.Add(this.tpinventario2);
+            this.Name = "Inventario";
+            this.Text = "Inventario";
+            this.Load += new System.EventHandler(this.Inventario_Load);
+            this.tpinventario2.ResumeLayout(false);
+            this.tpinvntario1.ResumeLayout(false);
+            this.tpinvntario1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.gproductos.ResumeLayout(false);
+            this.gproductos.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tpinventario2;
+        private System.Windows.Forms.TabPage tpinvntario1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStrip Menu;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel lblnombre;
+        private System.Windows.Forms.ToolStripLabel lblrol;
+        private System.Windows.Forms.ToolStripButton btnsalir;
+        private System.Windows.Forms.ToolStripButton btnconfiguracion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtproducto;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox gproductos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtdescripcion;
+        private System.Windows.Forms.DateTimePicker dtfecha_ven;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cbxclasificacion;
+        private System.Windows.Forms.ComboBox cbxmarca;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAgragar;
+    }
+}
